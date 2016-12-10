c47b14882c91d911be8df7bca3192598d0713fe5
Henry Middleton <h78900@gmail.com>
2016-12-10 10:33:54 -0500

    Python problem 3

---
+++ Python/problem3.py
@@ -0,0 +1,15 @@
+def find_smallest_factor(num):
+    for potential_factor in range(2, int(num/2)+1):
+        if num%potential_factor==0:
+            return potential_factor
+    return num
+
+def find_prime_factors(num):
+    factors = []
+    factor = find_smallest_factor(num)
+    while num != factor:#while num isn't prime
+        factors.append(factor)
+        num /= factor
+        factor = find_smallest_factor(num)
+    factors.append(int(factor))
+    return factors
