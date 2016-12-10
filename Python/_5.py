7c87e4a246183ce9c9138def2053323f9c5777ed
Henry Middleton <h78900@gmail.com>
2016-12-10 12:45:07 -0500

    Python problem 5

---
+++ Python/_5.py
@@ -0,0 +1,10 @@
+def find_smallest_product(max_factor):
+    if max_factor == 1:
+        return 1
+    else:
+        previous = find_smallest_product(max_factor-1)
+        candidate = previous
+        while True:
+            if candidate % max_factor == 0:
+                return candidate
+            candidate += previous
