7ba0f839f0bd86f58292e29ed8a543d46e45429f
Henry Middleton <h78900@gmail.com>
2016-12-10 16:43:57 -0500

    Add common resources file

--- Python/_7.py
+++ Python/_7.py
@@ -1,11 +1,2 @@
-def sieve_of_eratosthenes(max_num):
-    candidates = [n for n in range(3, max_num+1)]
-    primes = [2]
-    while primes[-1] != candidates[-1]:
-        for n in candidates:
-            if n%primes[-1] == 0:
-                candidates.remove(n)
-        primes.append(candidates[0])
-    return primes
-
+from common import sieve_of_eratosthenes
 print(sieve_of_eratosthenes(150000)[10000])
---
+++ Python/common.py
@@ -0,0 +1,9 @@
+def sieve_of_eratosthenes(max_num):
+    candidates = [n for n in range(3, max_num+1)]
+    primes = [2]
+    while primes[-1] != candidates[-1]:
+        for n in candidates:
+            if n%primes[-1] == 0:
+                candidates.remove(n)
+        primes.append(candidates[0])
+    return primes
