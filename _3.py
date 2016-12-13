26a99a115a33e70fb92a5980f4bd16703f8432fd
Henry Middleton <h78900@gmail.com>
2016-12-13 12:09:21 -0500

    Restructuring of common, problem 3

--- _3.py
+++ _3.py
@@ -1,17 +1,3 @@
-def find_smallest_factor(num):
-    for potential_factor in range(2, int(num/2)+1):
-        if num%potential_factor==0:
-            return potential_factor
-    return num
-
-def find_prime_factors(num):
-    factors = []
-    factor = find_smallest_factor(num)
-    while num != factor:#while num isn't prime
-        factors.append(factor)
-        num /= factor
-        factor = find_smallest_factor(num)
-    factors.append(int(factor))
-    return factors
+from common import find_prime_factors
 
 print(find_prime_factors(600851475143)[-1])
--- common.py
+++ common.py
@@ -1,13 +1,29 @@
 import math, time
 
+def find_smallest_factor(num):
+    for potential_factor in range(2, int(num/2)+1):
+        if num%potential_factor==0:
+            return potential_factor
+    return num
+
+def find_prime_factors(num):
+    factors = []
+    factor = find_smallest_factor(num)
+    while num != factor:#while num isn't prime
+        factors.append(factor)
+        num /= factor
+        factor = find_smallest_factor(num)
+    factors.append(int(factor))
+    return factors
+    
 def sieve_of_eratosthenes(max_num):#v fast right now
     primes = []
-    b = {n: True for n in range(2, max_num)}
+    candidates = {n: True for n in range(2, max_num)}
     for p in range(2, max_num):
-        if b[p]:
+        if candidates[p]:
             primes.append(p)
             for n in range(p, max_num, p):
-                b[n] = False
+                candidates[n] = False
     return primes
 
 def sieve_of_sundaram(max_num):#v slow right now, consult primer
