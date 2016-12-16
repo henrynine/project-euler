733ba6a8c672262959925a199b374e90c03566c5
Henry Middleton <h78900@gmail.com>
2016-12-16 13:51:30 -0500

    15 solved

--- _15.py
+++ _15.py
@@ -1,18 +1,6 @@
-'20 steps down, 20 steps right total'
-'''
-1x1: RD, DR
-2x2: RRDD, RDRD, RDDR, DRRD, DRDR, DDRR
-3x3: RRRDDD, RDRRDD, RDDRRD, RDDDRR, RRDRDD, RRDDRD, RRDDDR,
+from math import factorial as f
 
-'''
-def num_grid_paths(side_length):
-    if side_length == 1:
-        return 2
-    '''
-    mirror:
-    total edge: 1
-    in between: (s*(s-2)?
-    recursion: ngp(s-1)
-    '''
-    return 2*(1 + (side_length*(side_length-2)) 2*num_grid_paths(side_length-1))
-print(num_grid_paths(20))
+def combinations(n, r):
+    return int(f(n) / f(r) / f(n-r))
+    
+print(combinations(40, 20))
