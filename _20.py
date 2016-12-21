b2eda2b305c0cded60df07965dab54b76ab8cd34
Henry Middleton <h78900@gmail.com>
2016-12-21 08:50:25 -0500

    Problem 20 condensed

--- _20.py
+++ _20.py
@@ -1,2 +1,2 @@
 import math, functools
-print(functools.reduce(lambda x, y: x + y, map(int, list(str(math.factorial(100))))))
+print(functools.reduce(lambda x,y:x+y,map(int,list(str(math.factorial(100))))))
