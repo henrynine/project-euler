7529b171634ad8721f265dad52833ffd7b57c367
Henry Middleton <h78900@gmail.com>
2016-12-21 20:22:46 -0500

    22 solved

--- _22.py
+++ _22.py
@@ -1,2 +1,13 @@
 with open('resources/project_euler_22.txt', 'r') as names_raw:
-    name = names_raw.split(',')
+    names = names_raw.read().split(',')
+
+names = [name[1:-1] for name in names]
+names.sort()
+
+score = 0
+
+for i in range(len(names)):
+    for char in names[i]:
+        score += (ord(char) - ord('A') + 1) * (i+1)
+
+print(score)
