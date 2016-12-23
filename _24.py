26324c7212937e6bb44c25f0049446a49904f641
Henry Middleton <h78900@gmail.com>
2016-12-23 12:11:55 -0500

    24 solved

--- _24.py
+++ _24.py
@@ -5,14 +5,18 @@ get millionth
 '''
 import common, functools, operator
 
-digits = [str(n) for n in range(10)]
-
-permutations = []
-for b in common.get_binaries(10)
-
 def permutations(n):
-    digits = [str(n) for n in range(n)]
-    
+    permutation_list = []
+    this_digit = str(n-1)
+    if n == 1:
+        return ['0']
+    #insert n-1 digit into each spot of permuation below
+    for p in permutations(n-1):
+        for i in range(n):
+            #insert at i
+            permutation_list.append(p[:i] + this_digit + p[i:])
+    return permutation_list
 
-
-print(original_str)
+p = permutations(10)
+p.sort()
+print(p[999999])
